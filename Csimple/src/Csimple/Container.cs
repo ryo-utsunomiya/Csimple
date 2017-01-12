2a4028b853ff1d4a9d7301137c18b417e7dd7d13
Ryo Utsunomiya <millionsnowliving@gmail.com>
2017-01-13 01:08:52 +0900

    Initialize project

---
+++ Csimple/src/Csimple/Container.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Csimple
+{
+    public class Container
+    {
+        private readonly Dictionary<string, object> _dictionary;
+
+        public Container(Dictionary<string, object> dictionary = null)
+        {
+            _dictionary = dictionary ?? new Dictionary<string, object>();
+        }
+
+        public void Add(string key, object value)
+        {
+            _dictionary.Add(key, value);
+        }
+
+        public void Remove(string key)
+        {
+            _dictionary.Remove(key);
+        }
+
+        public bool Has(string key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public object Get(string key)
+        {
+            return _dictionary[key];
+        }
+    }
+}
